c4a1914b68bc92074da360190128ddef83a37374
Dimitar Georgiev <dimitar.georgiev@hotmail.com>
2015-05-25 01:00:41 +0300

    Added Scoreboard

--- ConsoleGame/ConsoleGame/Scoreboard.cs
+++ ConsoleGame/ConsoleGame/Scoreboard.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace ConsoleGame
+namespace Scoreboard
 {
     class Scoreboard
     {
+        
+
+            string newName = Console.ReadLine();
+            int newScore = int.Parse(Console.ReadLine());
+
+            string newData = newName + ":" + newScore;
+
+            string[] scores = File.ReadAllLines("../../Scoreboard.txt");
+            var orderedScores = scores.OrderByDescending(x => int.Parse(x.Split(':')[1]));
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                var lastScore = Regex.Match(scores[4], @"\d+").Value;
+                int extractedScore = Int32.Parse(lastScore);
+                if (newScore > extractedScore)
+                {
+                    scores[4] = newData;
+                }
+            }
+
+            foreach (var score in orderedScores)
+            {
+                Console.WriteLine(score.PadLeft(10, ' '));
+            }
+
+            File.WriteAllLines("../../Scoreboard.txt", orderedScores);
+
     }
 }
