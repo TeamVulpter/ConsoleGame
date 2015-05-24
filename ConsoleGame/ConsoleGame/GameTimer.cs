c296e9e2eb6c166e22256cc2e0230475716e9ad5
Eugeenek <eugeenk@gmail.com>
2015-05-24 21:13:38 +0300

    GameTimer

---
+++ ConsoleGame/ConsoleGame/GameTimer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Timers;
+
+namespace TimerExample
+{
+    class GameTmer
+    {
+        static Timer timer = new Timer(1000);
+        static int i = 10;
+
+        public static void Countdown()
+        {
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+            Console.Read();
+        }
+
+        private static void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            i--;
+            Console.Clear();
+            Console.WriteLine("Time Remaining:  " + i.ToString());
+
+            if (i == 0)
+            {
+                Console.WriteLine("G A M E  O V E R");
+                timer.Close();
+                timer.Dispose();
+            }
+            GC.Collect();
+        }
+    }
+}
